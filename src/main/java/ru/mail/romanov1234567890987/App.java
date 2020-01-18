0cca598c0b8f8492365a3e9244bc81ebed0e3ad8
Eltay750505 <55701366+Eltay750505@users.noreply.github.com>
2020-01-18 16:47:24 +0300

    added empty maven project

---
+++ src/main/java/ru/mail/romanov1234567890987/App.java
@@ -0,0 +1,13 @@
+package ru.mail.romanov1234567890987;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
